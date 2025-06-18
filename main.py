7ebb1e46c45a4a91a4efb062c47679b4bc586559
ASO <Alvarito1624@gmail.com>
2025-06-18 05:12:10 -0400

    procesamiento de la imagen

--- main.py
+++ main.py
@@ -20,11 +20,32 @@ async def procesar_imagen(file: UploadFile = File(...)):
         raise HTTPException(status_code=400, detail=f"Error al abrir la imagen: {e}")
 
     # Procesar imagen: convertir a escala de grises
-    imagen_gris = imagen.convert("L")
+    # imagen_gris = imagen.convert("L")
+#---------------------------
+    from skimage import io, measure, color,filters
+    import numpy as np
+    import cv2
+
+    # image = cv2.imread('../imagenes/Habitacion2.jpg')
+    image=imagen
+    gray_image= cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    _,binarizada=cv2.threshold(gray_image,127,255,cv2.THRESH_BINARY_INV)
+    labels = measure.label(binarizada ,connectivity=1)
+    borders = np.max(labels)
+
+    border_muestra = 1618
+
+    if borders >= border_muestra-5 and borders <= border_muestra+5 :
+        print(f"La imagen tiene {borders} bordes, lo cual significa que la habitacion esta limpia")
+    else:
+        print(f"La imagen tiene {borders} bordes, lo cual significa que la habitacion esta desordenada")
+
+#---------------------------
+
 
     # Guardar la imagen procesada en un buffer
     buffer = io.BytesIO()
-    imagen_gris.save(buffer, format="PNG")
+    gray_image.save(buffer, format="PNG")
     buffer.seek(0)
 
     return StreamingResponse(buffer, media_type="image/png")
